83d883a61ed95ba7afa74c72dc654bf2f1e1d9f3
Phil S <philipstubbs13@gmail.com>
2018-01-28 09:25:18 -0600

    got movies displaying correctly. Need to figure out how to display movie when no movie is specified

--- liri.js
+++ liri.js
@@ -21,41 +21,59 @@ require("dotenv").config();
 //var spotify = new Spotify(keys.spotify);
 //var client = new Twitter(keys.twitter);
 
+
 //If the liriCommand is movie-this and a movieName is provided...
 //Output information about that movie.
-if (liriCommand === "movie-this" && movieName != null) {
+if (liriCommand === "movie-this") {
 	getMovieInfo();
-	console.log(movieName);
 }
-
 
 //Get movie info function... Run this function to get movie info for the specified movie.
 function getMovieInfo() {
+
 	//Then, run a request to the OMDB API with the movieName the user enters.
 	request("http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy", function(error, response, body) {
 
+		//Create variable to hold all the movie info that we will output to the console.
+		//Parse the body of the JSON object that holds the movie data and display the movie info.
+		var movieInfo = JSON.parse(body);
+		//Title of movie
+		var movieTitle = "Title: " + movieInfo.Title;
+		//Year the movie came out.
+		var movieYear = "Year movie was released: " + movieInfo.Year;
+		//IMDB Rating of the movie.
+		var IMDBRating = "IMDB movie rating (out of 10): " + movieInfo.imdbRating;
+		//Rotten Tomatoes rating of the movie.
+		var rottenTomatoes = "Rotten Tomatoes rating (out of 100%): " + movieInfo.Ratings[1].Value;
+		//Country where the movie was produced.
+		var countryProduced = "Filmed in: " + movieInfo.Country;
+		//Language of the movie.
+		var movieLanguage = "Language: " + movieInfo.Language;
+		//Plot of the movie.
+		var moviePlot = "Movie plot: " + movieInfo.Plot;
+		//Actors in the movie.
+		var movieActors = "Actors: " + movieInfo.Actors;
+
 		//If the request is successful (i.e. if the response status code is 200)
 		if (!error && response.statusCode === 200) {
-			//Parse the body of the JSON object that holds the movie data and display the movie info.
-			var movieInfo = JSON.parse(body);
 			//console.log(JSON.parse(body));
 			//Output the following information to terminal window.
 			//Title of the movie.
-			console.log("Title: " + movieInfo.Title);
+			console.log(movieTitle);
 		   	//Year the movie came out.
-		   	console.log("Year movie was released: " + movieInfo.Year);
+		   	console.log(movieYear);
 		   	//IMDB Rating of the movie.
-		   	console.log("IMDB movie rating (out of 10): " + movieInfo.imdbRating);
+		   	console.log(IMDBRating);
 		   	//Rotten Tomatoes rating of the movie.
-		   	console.log("Rotten Tomatoes rating (out of 100%): " + movieInfo.Ratings[1].Value);
+		   	console.log(rottenTomatoes);
 		   	//Country where the movie was produced.
-		   	console.log("Filmed in: " + movieInfo.Country);
+		   	console.log(countryProduced);
 		   	//Language of the movie.
-		   	console.log("Language: " + movieInfo.Language);
+		   	console.log(movieLanguage);
 		   	//Plot of the movie.
-		   	console.log("Movie plot: " + movieInfo.Plot);
+		   	console.log(moviePlot);
 		   	//Actors in the movie.
-		   	console.log("Actors: " + movieInfo.Actors);
+		   	console.log(movieActors);
 		}
 	});
 }
